6c46818e191f41da33c34c7e1e8496808c6537b3
cobra-r9 <cobra.rev.9@gmail.com>
2026-07-05 22:48:12 +0530

    slice.c datatype in c

---
+++ misc/arrays/slice.c
@@ -0,0 +1,98 @@
+// name : slice.c 
+// date : 2026-07-05 
+// std  : C 23 standard 
+// desc : a custom c slice type (ingeligent strings)
+
+//---------------------------------- include header files -------------------------------
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <stdbool.h>
+
+// define the slice type. 
+// use the anony structs. 
+typedef struct {
+    char *data;
+    size_t len;
+} slice;
+
+// a custom print_slice function to print the slice. 
+void print_slice(slice string) {
+    for (size_t i = 0; i < string.len; i++) {
+        putchar(string.data[i]);
+    }
+}
+
+// a custom puts_slice function to include a newline at end. 
+void puts_slice(slice string) {
+    for (size_t i = 0; i < string.len; i++) {
+        putchar(string.data[i]);
+    }
+    putchar('\n');
+}
+
+// a custom slice_from function for converting the c string into slice. 
+slice slice_from(char *string) {
+    return (slice){.data = string, .len = strlen(string)};
+}
+
+
+// a custom function iseq_slice to check if two slices are equal. 
+bool iseq_slice(slice s1, slice s2) {
+    if (s1.len != s2.len) return false;
+    for (size_t i = 0; i < s2.len; i++) {
+        if (s1.data[i] != s2.data[i]) return false;
+    }
+    return true;
+}
+
+// a cunstom function slice_startswith to check if the slice starts with a given prefix. 
+// if slice s1 starts with s2
+bool slice_startswith(slice s1, slice s2) {
+    if (s1.len < s2.len) return false;
+    for (size_t i = 0; i < s2.len; i++) {
+        if (s1.data[i] != s2.data[i]) return false;
+    }
+    return true;
+}
+
+// a custom function slice_from_n, returns first n bytes of slice, returns a slice. 
+slice slice_from_n(slice slc, size_t n) {
+    if (slc.len < n) n = slc.len;
+    return (slice){.data = slc.data, .len = n};
+
+}
+
+
+//--------------------------------------- main program ----------------------------------
+int main(void) {
+
+    char *string = "this is a c string";
+
+    // slc0
+    slice slc0 = {.data = string, .len = strlen(string)};
+
+    printf("The data is @ : %p and has value %s, with length : %zu\n", slc0.data, slc0.data, slc0.len);
+    printf("The string - slc0 is : ");
+    print_slice(slc0); 
+    putchar('\n');
+
+    slice slc1 = slice_from("This is a C slice.");
+    puts_slice(slc1);
+
+    slice slc2 = slice_from("This is a C slice.");
+    if (iseq_slice(slc1, slc2)) {
+        puts("slc1 and slc2 are equal.");
+    }
+
+    if (slice_startswith(slice_from("myvariable=something"), slice_from("myvariable"))) {
+        printf("%s starts with %s\n", "myvariable=something", "myvariable");
+    }
+
+    slice slc3 = slice_from_n(slc2, 4);
+    puts_slice(slc3);
+
+    return EXIT_SUCCESS;
+
+}
+
