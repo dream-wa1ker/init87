af401be8264cf3986b35a3ab476bccd9b363703c
cobra-r9 <cobra.rev.9@gmail.com>
2026-07-05 11:21:46 +0530

    initialised fgets

---
+++ misc/stdio/fgets.c
@@ -0,0 +1,17 @@
+// name : fgets.c 
+// date : 2026-07-05 
+// std  : C 23 standard 
+
+
+//---------------------------------- include header files -------------------------------
+#include <stdio.h> 
+#include <stdlib.h> 
+
+
+//--------------------------------------- main program ----------------------------------
+int main(void) {
+
+    puts("");
+    return EXIT_SUCCESS;
+}
+
