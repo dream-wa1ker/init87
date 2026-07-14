316dfa5f21d77d9b9c9f4cbcafa1eebc596a7ea8
cobra-r9 <cobra.rev.9@gmail.com>
2026-07-14 09:40:42 +0530

    revised errors : snprintf

--- misc/stdio/errors/snprintf.c
+++ misc/stdio/errors/snprintf.c
@@ -9,9 +9,17 @@
 
 //--------------------------------------- main program ----------------------------------
 int main(void) {
+
+    // this is a buffer overflow.    
     char buffer[4];
     snprintf(buffer, 5, "%d%d%d%d", 1, 2, 3, 4);
     printf("%s\n", buffer);
+
+    // this is a truncation.
+    char data[4];
+    snprintf(data, 4, "%d%d%d%d", 1, 2, 3, 4);
+    printf("%s\n", data);
     return 0;
 }
+
 
